a3bdd8d3ac4f70a8d31b696b3510cb791cd870cf
rackyjethava <raktpx2356@gmail.com>
2024-08-21 08:54:19 +0530

    21-8-change

--- src/index.js
+++ src/index.js
@@ -8,14 +8,35 @@ const cors = require('cors')
 
 const passport = require("passport")
 const {Googlelogin,facebooklogin}=require("./utils/provider")
-const connectchat = require("./utils/soketIO")
+// const connectchat = require("./utils/soketIO")
 YAML = require('yamljs');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = YAML.load('./src/api.yaml');
-
+// const swaggerDocument = YAML.load('./src/api.yaml');
 
 
 app.use(cookieParser())
+Googlelogin()
+facebooklogin()
+const _dirname = path.resolve();
+
+const __swaggerDistPath = path.join(_dirname, 'node_modules', 'swagger-ui-dist'); //install swagger-ui-dist
+
+const swaggerDocument = YAML.load(path.resolve('./public', 'api.yaml'));
+
+
+app.use(
+    '/api/docs',
+    express.static(__swaggerDistPath, { index: false }), // Serve Swagger UI assets
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument, {
+        swaggerOptions: {
+            url: '/public/api.yaml' // Path to your YAML file
+        }
+    })
+);
+
+
+connectDB()
 app.use(cors({
    origin: "http://localhost:3000",
    credentials: true,
@@ -32,13 +53,12 @@ app.use(passport.session());
 app.use(passport.initialize());
 
 app.use("/api/v1", routs)
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-
-connectDB()
-Googlelogin()
-facebooklogin()
-connectchat()
+// connectchat()
+app.use('/', (req, res) => {
+   res.send('hello world')
+})
 
 app.listen(8000, () => {
    console.log("server is running on port 8000")
