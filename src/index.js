0746c10bd28fb22ff317d150e3b7fb175d1bca73
princychodvadiya <princychodvadiya721@gmail.com>
2024-08-22 08:22:41 +0530

    22-8-corse

--- src/index.js
+++ src/index.js
@@ -37,10 +37,22 @@ app.use(
 
 
 connectDB()
+const allowedOrigins = [
+   'https://fruitable-client-iota.vercel.app',
+   'https://fruitable-client-n8orv05ao-rakeshs-projects-cbf5c5c9.vercel.app'
+];
+
 app.use(cors({
-   origin: "https://fruitable-client-iota.vercel.app",
-   credentials: true,
-}))
+  origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+          callback(null, true);
+      } else {
+          callback(new Error('Not allowed by CORS'));
+      }
+  },
+  credentials: true,
+}));
+
 app.use(express.json())
 app.use(require('express-session')
    ({
